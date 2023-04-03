16b48ef772a3cf496a88b1ce13ab6825493f8a21
adrioblea850 <adrioblea850@gmail.com>
2023-04-03 15:45:44 -0600

    modificaciones al fragment ajustes

--- app/src/main/java/com/example/didactickids/FragmentAjustes.java
+++ app/src/main/java/com/example/didactickids/FragmentAjustes.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,7 +59,14 @@ public class FragmentAjustes extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ajustes, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_ajustes, container, false);
+        ImageButton imgPerfil = rootView.findViewById(R.id.profile);
+        ImageButton ajustesusu= rootView.findViewById(R.id.usersettings);
+        ImageButton ajusteslec = rootView.findViewById(R.id.lessonsettings);
+        ImageButton ayuda = rootView.findViewById(R.id.help);
+        ImageButton cerrarSecion = rootView.findViewById(R.id.logout);
+        // ...
+        return rootView;
     }
 }
