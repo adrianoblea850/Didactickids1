21781f51883be69b00b577df0d0eb344d0aa036d
adrioblea850 <adrioblea850@gmail.com>
2023-04-03 15:30:09 -0600

    modificaciones al fragment perfil y al archivo java

--- app/src/main/java/com/example/didactickids/FragmentPerfil.java
+++ app/src/main/java/com/example/didactickids/FragmentPerfil.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +15,9 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class FragmentPerfil extends Fragment {
+
+
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -44,6 +48,7 @@ public class FragmentPerfil extends Fragment {
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
+
     }
 
     @Override
@@ -53,12 +58,19 @@ public class FragmentPerfil extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_perfil, container, false);
+        ImageButton imgPerfil = rootView.findViewById(R.id.profile);
+        ImageButton imgdots = rootView.findViewById(R.id.dots);
+        ImageButton imglecciones = rootView.findViewById(R.id.lecciones);
+        // ...
+        return rootView;
     }
+
 }
