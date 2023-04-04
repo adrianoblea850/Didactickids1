8d6a8d74889c114fe3fb2c58cafd3f09ff4d5f93
Karen-Espinosa <kacesgu141201@gmail.com>
2023-04-04 15:38:58 -0600

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/didactickids/FragmentHome.java
+++ app/src/main/java/com/example/didactickids/FragmentHome.java
@@ -7,11 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-public class FragmentLeccion extends Fragment {
+
+public class FragmentHome extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_leccion, container, false);
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
