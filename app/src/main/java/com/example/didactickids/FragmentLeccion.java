000d56c095f36052e99cc9cfcf8b823f84dd177f
Luz Maria <gonzalezgalloluzmaria20@gmail.com>
2023-04-14 22:44:37 -0600

    Diseño en proceso

--- app/src/main/java/com/example/didactickids/FragmentLeccion.java
+++ app/src/main/java/com/example/didactickids/FragmentLeccion.java
@@ -1,17 +1,42 @@
 package com.example.didactickids;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView
+
+import java.net.URI;
+
 public class FragmentLeccion extends Fragment {
+
+    private TextView t;
+    private Button _bnt_suma;
+    String _url="https://www.youtube.com/watch?v=eLoJWiucZJE";
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_leccion, container, false);
+
+        _bnt_suma = _bnt_suma.findViewById();
+
+        _bnt_suma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri _link = Uri.parse(_url);
+                Intent i = new Intent(Intent.ACTION_VIEW,_link);
+                startActivity(i);
+
+                //Log.d("Click", "ok");
+            }
+        });
     }
 }
