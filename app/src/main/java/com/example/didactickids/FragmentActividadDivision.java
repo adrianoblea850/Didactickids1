28ac6407fdca9166af7dadb7091a0d599b3599bd
Karen-Espinosa <kacesgu141201@gmail.com>
2023-04-17 23:28:06 -0600

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/didactickids/FragmentActividadDivision.java
+++ app/src/main/java/com/example/didactickids/FragmentActividadDivision.java
@@ -7,58 +7,48 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentActividadDivision#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FragmentActividadDivision extends Fragment {
+    EditText divnum1,divnum2;
+    TextView tvresultadodivision;
+    private int num1 = 540;
+    private int num2 = 5;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public FragmentActividadDivision() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentActividadDivision.
-     */
-    // TODO: Rename and change types and number of parameters
     public static FragmentActividadDivision newInstance(String param1, String param2) {
         FragmentActividadDivision fragment = new FragmentActividadDivision();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_actividad_division, container, false);
+        View view = inflater.inflate(R.layout.fragment_actividad_division, container, false);
+        divnum1 =  view.findViewById(R.id.divnum1);
+        divnum2 = view.findViewById(R.id.divinum2);
+        tvresultadodivision =  view.findViewById(R.id.tvresultadodivision);
+        return view;
+    }
+    public void verificar(View view){
+        int num1_input = Integer.parseInt(divnum1.getText().toString());
+        int num2_input = Integer.parseInt(divnum2.getText().toString());
+        if (num1_input == num1 && num2_input == num2) {
+            tvresultadodivision.setText("135 Respuesta Correcta");
+        } else {
+            tvresultadodivision.setText("Respuesta Incorrecta");
+        }
+
     }
 }
--- app/src/main/java/com/example/didactickids/FragmentActividadMultiplicacion.java
+++ app/src/main/java/com/example/didactickids/FragmentActividadMultiplicacion.java
@@ -7,41 +7,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentActividadMultiplicacion#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FragmentActividadMultiplicacion extends Fragment {
+    EditText multinum1,multinum2;
+    TextView tvresultadomulti;
+    private int num1 = 5;
+    private int num2 = 4;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public FragmentActividadMultiplicacion() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentActividadMultiplicacion.
-     */
-    // TODO: Rename and change types and number of parameters
     public static FragmentActividadMultiplicacion newInstance(String param1, String param2) {
         FragmentActividadMultiplicacion fragment = new FragmentActividadMultiplicacion();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +31,26 @@ public class FragmentActividadMultiplicacion extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_actividad_multiplicacion, container, false);
+        View view = inflater.inflate(R.layout.fragment_actividad_multiplicacion, container, false);
+        multinum1 =  view.findViewById(R.id.multinum1);
+        multinum2 = view.findViewById(R.id.multinum2);
+        tvresultadomulti =  view.findViewById(R.id.tvresultadomulti);
+        return view;
+    }
+    public void verificar(View view){
+        int num1_input = Integer.parseInt(multinum1.getText().toString());
+        int num2_input = Integer.parseInt(multinum2.getText().toString());
+        if (num1_input == num1 && num2_input == num2) {
+            tvresultadomulti.setText("20 Respuesta Correcta");
+        } else {
+            tvresultadomulti.setText("Respuesta Incorrecta");
+        }
+
     }
 }
