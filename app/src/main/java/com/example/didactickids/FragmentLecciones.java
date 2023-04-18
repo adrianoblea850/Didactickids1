ee25f47c9d0c6ed4c7d78e92eb9fec06c3fc513b
adrioblea850 <adrioblea850@gmail.com>
2023-04-18 00:27:27 -0600

    xsd

--- app/src/main/java/com/example/didactickids/FragmentLecciones.java
+++ app/src/main/java/com/example/didactickids/FragmentLecciones.java
@@ -1,18 +1,37 @@
 package com.example.didactickids;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 public class FragmentLecciones extends Fragment {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lecciones, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_leccion, container, false);
+        Button _btn_suma = rootView.findViewById(R.id.btn_suma);
+        _btn_suma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, new fragment_leccion_suma());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        return  rootView;
     }
 }
