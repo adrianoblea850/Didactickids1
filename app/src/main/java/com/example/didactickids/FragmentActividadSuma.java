fffe19afe8a956bdc6c46b6a86c5b9e7c0935f38
Luz Maria <gonzalezgalloluzmaria20@gmail.com>
2023-04-17 20:24:28 -0600

    Diseño en proceso

--- app/src/main/java/com/example/didactickids/FragmentActividadSuma.java
+++ app/src/main/java/com/example/didactickids/FragmentActividadSuma.java
@@ -1,4 +1,4 @@
-package com.example.didactickids;
+/*package com.example.didactickids;
 
 import android.os.Bundle;
 
@@ -12,12 +12,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public abstract class MainActivity extends AppCompatActivity implements  View.OnClickListener{
+public  class FragmentActividadSuma extends Fragment implements  View.OnClickListener{
     EditText txt1, txt2;
     Button btnCalcular;
     TextView txtResultados;
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txt1=(EditText) findViewById(R.id.txt1);
@@ -34,4 +34,51 @@ public abstract class MainActivity extends AppCompatActivity implements  View.On
 
         txtResultados.setText("La suma es: "+r);
     }
+}*/
+package com.example.didactickids;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class FragmentActividadSuma extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_actividad_suma, container, false);
+        EditText txt1 = rootView.findViewById(R.id.txt1);
+        EditText txt2 = rootView.findViewById(R.id.txt2);
+        TextView txtResultados= rootView.findViewById(R.id.txtResultados);
+        Button btnCalcular= rootView.findViewById(R.id.bntCalcular);
+
+
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int valor1 = Integer.parseInt(txt1.getText().toString());
+                int valor2 = Integer.parseInt(txt2.getText().toString());
+                int r = valor1 + valor2;
+
+                txtResultados.setText("La suma es: "+r);
+
+            }
+
+
+
+        });
+        return rootView;
+
+
+    }
+
 }
